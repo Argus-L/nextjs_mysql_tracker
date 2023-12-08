c142508ea9756a883afba4f86524cf4b29890b3e
Argus-L <91214444+Argus-L@users.noreply.github.com>
2023-12-08 18:53:33 -0500

    Fixed the bug, cleaned up code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import prisma from "../lib/prisma";
 import DeleteButton from "@/components/DeleteButton";
-//import RouteConfig from "@/components/RouteConfig";
 export const dynamic = "force-dynamic";
 export const revalidate = "0";
 
